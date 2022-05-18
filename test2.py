3d8bb2fff412b3d6fc58798f0f5edba474f7acfa
arang <paco.arango@ensta-bretagne.org>
2022-05-18 08:39:18 +0200

    acquisition données

---
+++ test2.py
@@ -0,0 +1,28 @@
+# Simple example use of using pyGPS
+import test
+import time
+
+# Setup pyGPS with a baud rate of 4800
+gps = test.pyGPS(4800)
+file = f"gps_export_{time.time()}.txt"
+
+while True:
+    # gps.updateData()
+
+    raw_data = gps.m_serial.readline()
+    data_line = raw_data.decode('utf-8')
+    data = data_line.split(",")
+    # if (data[0] == "$GPGGA"):
+    #             f = open('DONNEES_GPS.txt','w')
+    #             for element in data:
+    #                 f.write(element+",")
+    #             f.write("\n")
+    print(data)
+    with open(file, 'a') as f:
+        f.write(str(data).replace('[', '').replace(']', '') + "\n")
+
+    time.sleep(2)
+    gps.printData()
+
+
+
